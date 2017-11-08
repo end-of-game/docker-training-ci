15ee178b730e5b285acd4a83a58fd68247cf3be8
w.bartlett <w.bartlett@treeptik.fr>
2017-11-08 14:20:16 +0100

    Initial commit

---
+++ Jenkinsfile
@@ -0,0 +1,17 @@
+pipeline {
+	agent any
+	triggers { pollSCM('*/1 * * *') }
+	
+	stages {
+		stage('checkout') {
+			steps {
+				checkout scm
+			}
+		}
+		stage('build & publish') {
+			steps {
+				sh 'docker run -it --rm -v.:. maven:3.5.2-jdk-8-alpine mvn --settings settings.xml compile'
+			}
+		}
+	}
+}
---
+++ src/main/java/com/example/helloworld/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.helloworld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+	
+	@GetMapping
+	@ResponseBody
+	public String sayHello() {
+		return "Hello, World!";
+	}
+}
